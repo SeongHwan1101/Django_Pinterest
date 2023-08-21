9b7e5e30fc380745212da8852d675173185e1fbb
SeongHwan1101 <2020270810@korea.ac.kr>
2023-08-21 21:35:27 +0900

    Django course 14 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -131,9 +131,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     BASE_DIR / "static",
-}
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
