49ea862242b43e680afd054d121a286081a99e30
SeongHwan1101 <2020270810@korea.ac.kr>
2023-08-31 17:16:57 +0900

    Django course 21 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -29,6 +32,7 @@ environ.Env.read_env(
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
